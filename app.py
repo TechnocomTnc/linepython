6e7dfe49c13dac978a7800402c989cb186e081e3
TechnocomTnc <predapon.t@gmail.com>
2018-06-15 17:21:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,11 +41,11 @@ def bot():
     text = msg_in_json["events"][0]['message']['text'].lower().strip()
     
     # ตัวอย่างการทำให้ bot ถาม-ตอบได้ แบบ exact match
-      response_dict = {'Hi':'Hii'}
-      if text in response_dict:
-          replyQueue.append(reponse_dict[text])
-      else:
-          replyQueue.append('ไม่รู้ว่าจะตอบอะไรดี TT')
+    response_dict = {'Hi':'Hii'}
+    if text in response_dict:
+           replyQueue.append(reponse_dict[text])
+    else:
+           replyQueue.append('ไม่รู้ว่าจะตอบอะไรดี TT')
        
     # ตัวอย่างการทำให้ bot ถาม-ตอบได้ แบบ non-exact match
     # โดยที่มี method ชื่อ find_closest_sentence ที่ใช้การเปรียบเทียบประโยค
@@ -59,8 +59,8 @@ def bot():
     replyQueue.append('นี่คือรูปแบบข้อความที่รับส่ง')
     
     # ทดลอง Echo ข้อความกลับไปในรูปแบบที่ส่งไปมา (แบบ json)
-    replyQueue.append(msg_in_string)
-    reply(replyToken, replyQueue[:5])
+    # replyQueue.append(msg_in_string)
+    # reply(replyToken, replyQueue[:5])
     
     return 'OK', 200
  
