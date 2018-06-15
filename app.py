7970130f78749e5a80a9fb7aea0fe86b2e97bd6a
TechnocomTnc <predapon.t@gmail.com>
2018-06-15 17:24:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def bot():
     # ตรวจสอบว่า ที่ส่งเข้ามาเป็น text รึป่าว (อาจเป็น รูป, location อะไรแบบนี้ได้ครับ)
     # แต่ก็สามารถประมวลผลข้อมูลประเภทอื่นได้นะครับ
     # เช่น ถ้าส่งมาเป็น location ทำการดึง lat long ออกมาทำบางอย่าง เป็นต้น
-    if msgType != 'text':
+    if msgType != 'sticker':
         reply(replyToken, ['Only text is allowed.'])
         return 'OK',200
     
@@ -41,11 +41,11 @@ def bot():
     text = msg_in_json["events"][0]['message']['text'].lower().strip()
     
     # ตัวอย่างการทำให้ bot ถาม-ตอบได้ แบบ exact match
-    response_dict = {'Hi':'Hii'}
-    if text in response_dict:
-           replyQueue.append(reponse_dict[text])
-    else:
-           replyQueue.append('ไม่รู้ว่าจะตอบอะไรดี TT')
+    #   response_dict = {'Hi':'Hii'}
+    #   if text in response_dict:
+    #       replyQueue.append(reponse_dict[text])
+    #   else:
+    #       replyQueue.append('ไม่รู้ว่าจะตอบอะไรดี TT')
        
     # ตัวอย่างการทำให้ bot ถาม-ตอบได้ แบบ non-exact match
     # โดยที่มี method ชื่อ find_closest_sentence ที่ใช้การเปรียบเทียบประโยค
