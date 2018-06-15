cd02d4c34fc9983a8a04a94f1f69985d7eb3507d
TechnocomTnc <predapon.t@gmail.com>
2018-06-15 17:11:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 
 # ตรง YOURSECRETKEY ต้องนำมาใส่เองครับจะกล่าวถึงในขั้นตอนต่อๆ ไป
 global LINE_API_KEY
+# ห้ามลบคำว่า Bearer ออกนะครับเมื่อนำ access token มาใส่
 LINE_API_KEY = 'Bearer 7YR60AJ855Zu1Etxsc7aCdFqhip1o8yAKj7PzLe90ClE9Po0fz5o81BeghtpCki4+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDaDqdC5IHk30qnjo7GQ1hHKizexgGNpBPn/Fwz3slJqkQdB04t89/1O/w1cDnyilFU='
 
 app = Flask(__name__)
@@ -15,26 +16,53 @@ def index():
 
 def bot():
     # ข้อความที่ต้องการส่งกลับ
-    replyStack = list()
+    replyQueue = list()
    
     # ข้อความที่ได้รับมา
     msg_in_json = request.get_json()
     msg_in_string = json.dumps(msg_in_json)
     
     # Token สำหรับตอบกลับ (จำเป็นต้องใช้ในการตอบกลับ)
-    replyToken = msg_in_json["events"][0]['replyToken']   
+    replyToken = msg_in_json["events"][0]['replyToken']
     
-    # ทดลอง Echo ข้อความกลับไปในรูปแบบที่ส่งไป-มา (แบบ json)
-    replyStack.append("เทสๆ")
-    replyStack.append("ปรับปรุงแปปครับ")
-    reply(replyToken, replyStack[:5])
- 
-    if msg_in_string == "ดี" :
-        replyStack.append("ดีครับ")
-        reply(replyToken, replyStack[:5])
+    # ส่วนนี้ดึงข้อมูลพื้นฐานออกมาจาก json (เผื่อ)
+    userID =  msg_in_json["events"][0]['source']['userId']
+    msgType =  msg_in_json["events"][0]['message']['type']
+    
+    # ตรวจสอบว่า ที่ส่งเข้ามาเป็น text รึป่าว (อาจเป็น รูป, location อะไรแบบนี้ได้ครับ)
+    # แต่ก็สามารถประมวลผลข้อมูลประเภทอื่นได้นะครับ
+    # เช่น ถ้าส่งมาเป็น location ทำการดึง lat long ออกมาทำบางอย่าง เป็นต้น
+    if msgType != 'text':
+        reply(replyToken, ['Only text is allowed.'])
+        return 'OK',200
+    
+    # ตรงนี้ต้องแน่ใจว่า msgType เป็นประเภท text ถึงเรียกได้ครับ 
+    # lower เพื่อให้เป็นตัวพิมพ์เล็ก strip เพื่อนำช่องว่างหัวท้ายออก ครับ
+    text = msg_in_json["events"][0]['message']['text'].lower().strip()
+    
+    # ตัวอย่างการทำให้ bot ถาม-ตอบได้ แบบ exact match
+    # response_dict = {'สวัสดี':'สวัสดีครับ'}
+    # if text in response_dict:
+    #     replyQueue.append(reponse_dict[text])
+    # else:
+    #     replyQueue.append('ไม่รู้ว่าจะตอบอะไรดี TT')
+       
+    # ตัวอย่างการทำให้ bot ถาม-ตอบได้ แบบ non-exact match
+    # โดยที่มี method ชื่อ find_closest_sentence ที่ใช้การเปรียบเทียบประโยค
+    # เพื่อค้นหาประโยคที่ใกล้เคียงที่สุด อาจใช้เรื่องของ word embedding มาใช้งานได้ครับ
+    # simple sentence embeddings --> https://openreview.net/pdf?id=SyK00v5xx
+    # response_dict = {'สวัสดี':'สวัสดีครับ'}
+    # closest = find_closest_sentence(response_dict, text)
+    # replyQueue.append(reponse_dict[closest])
    
-   
-    return 'OK',200
+    # ตอบข้อความ "นี่คือรูปแบบข้อความที่รับส่ง" กลับไป
+    replyQueue.append('นี่คือรูปแบบข้อความที่รับส่ง')
+    
+    # ทดลอง Echo ข้อความกลับไปในรูปแบบที่ส่งไปมา (แบบ json)
+    replyQueue.append(msg_in_string)
+    reply(replyToken, replyQueue[:5])
+    
+    return 'OK', 200
  
 def reply(replyToken, textList):
     # Method สำหรับตอบกลับข้อความประเภท text กลับครับ เขียนแบบนี้เลยก็ได้ครับ
