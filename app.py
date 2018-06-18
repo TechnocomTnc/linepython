d7d2a0a2305706e98188fbd43ec849d049646149
TechnocomTnc <predapon.t@gmail.com>
2018-06-18 17:05:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import MySQLdb
 import json
 dbconn=MySQLdb.connect(database='db_junebot', user='root', password='', host='localhost',charset='utf8')
 query = "select * from question"
- with dbconn.cursor(MySQLdb.cursors.DictCursor) as cursor:
+with dbconn.cursor(MySQLdb.cursors.DictCursor) as cursor:
     cursor.execute(query)
     data = cursor.fetchall()
 
