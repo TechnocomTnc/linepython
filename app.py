839d1688cc9aa426258796ed88edd197c15513af
TechnocomTnc <predapon.t@gmail.com>
2018-06-18 17:16:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,7 @@ import json
 import requests
 import random
 import MySQLdb
-import json
-dbconn=MySQLdb.connect(database='db_junebot', user='root', password='', host='localhost',charset='utf8')
-query = "select * from question"
-with dbconn.cursor(MySQLdb.cursors.DictCursor) as cursor:
-    cursor.execute(query)
-    data = cursor.fetchall()
+
 
 
 
@@ -53,6 +48,12 @@ def bot():
     replyQueue.append(text)
 
     
+    
+    dbconn=MySQLdb.connect(database='db_junebot', user='root', password='', host='localhost',charset='utf8')
+    query = "select * from question"
+    with dbconn.cursor(MySQLdb.cursors.DictCursor) as cursor:
+    cursor.execute(query)
+    data = cursor.fetchall()
     x = json.dumps(data,indent=4)
     replyQueue.append(x)
 
